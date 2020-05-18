b4390ff324c653bb224eb55b2c6d4f55d4fa4868
Se7endz <oussama.bendifallahd@gmail.com>
2020-05-18 23:53:44 +0200

    je crois que j ai fais nimp

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_205932) do
+ActiveRecord::Schema.define(version: 2020_05_18_210839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
